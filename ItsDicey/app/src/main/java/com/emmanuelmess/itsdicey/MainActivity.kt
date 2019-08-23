c2c395c61a5c609093338b9f6cb6b0bf98793a77
Emmanuel M <emmanuelbendavid@gmail.com>
2019-08-23 14:06:52 -0300

    Migrate to AndroidX

--- ItsDicey/app/src/main/java/com/emmanuelmess/itsdicey/MainActivity.kt
+++ ItsDicey/app/src/main/java/com/emmanuelmess/itsdicey/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.emmanuelmess.itsdicey
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-import android.view.MotionEvent
 import android.view.View.GONE
 import android.view.View.VISIBLE
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
